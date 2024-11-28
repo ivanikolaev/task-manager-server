3e01462317ac3bc801179f0c501615c77370f7be
ivanikolaev <goturpokla@gmail.com>
2024-11-28 19:24:32 +0300

    redoc added

--- app.ts
+++ app.ts
@@ -1,20 +1,36 @@
-import express, {Express, Request, Response} from 'express'
-import {db} from './database';
-import {serverStart} from "./startServer";
+import express, { Express, Request, Response } from 'express';
+import { db } from './database';
+import { serverStart } from "./startServer";
 import setCors from "./middlewares/CORS";
-import authRouter from './routes/authRoute'
+import authRouter from './routes/authRoute';
 import tasksRoute from "./routes/tasksRoute";
 import recordingRoute from './routes/recordingRoute';
+import path from 'path';
 
-const app: Express = express()
+const app: Express = express();
 const { swaggerUi, swaggerSpec } = require('./swagger');
+
+// Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(express.json())
-app.use(setCors)
-app.use('/auth', authRouter)
-app.use('/tasks', tasksRoute)
-app.use('/refresh', recordingRoute)
+
+// Redoc
+app.get('/redoc', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'redoc.html'));
+});
+
+// Redoc-spec JSON (для передачи спецификации в Redoc)
+app.get('/redoc-spec', (req: Request, res: Response) => {
+  res.json(swaggerSpec);
+});
+
+app.use(express.json());
+app.use(setCors);
+app.use('/auth', authRouter);
+app.use('/tasks', tasksRoute);
+app.use('/refresh', recordingRoute);
+
 app.get("/", (req: Request, res: Response) => {
-    res.send('hello')
-})
-serverStart(app, db)
+  res.send('hello');
+});
+
+serverStart(app, db);
